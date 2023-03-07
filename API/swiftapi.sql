4ed1e79657eef0faaf83bfe079b84bea100d938b
Pokoki <92927093+Pokoki@users.noreply.github.com>
2023-03-07 22:42:27 +0100

    Update swiftapi.sql

--- API/swiftapi.sql
+++ API/swiftapi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : mar. 07 mars 2023 à 20:09
+-- Généré le : mar. 07 mars 2023 à 21:29
 -- Version du serveur : 5.7.39
 -- Version de PHP : 7.4.33
 
@@ -20,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `swiftapi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `detail_sport`
+--
+
+CREATE TABLE `detail_sport` (
+  `id` int(11) NOT NULL,
+  `id_plan` int(11) DEFAULT NULL,
+  `exo_name` varchar(50) DEFAULT NULL,
+  `timer` time DEFAULT NULL,
+  `repetition` int(11) DEFAULT NULL,
+  `exo_set` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,12 +77,25 @@ CREATE TABLE `ingredient_detail` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `plan_sport`
+--
+
+CREATE TABLE `plan_sport` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `categories` varchar(40) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `recipes`
 --
 
 CREATE TABLE `recipes` (
   `id_user` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `name` char(50) NOT NULL,
   `timer` bigint(20) NOT NULL,
   `description` text NOT NULL,
   `id` int(11) NOT NULL
@@ -80,23 +108,29 @@ CREATE TABLE `recipes` (
 --
 
 CREATE TABLE `users` (
-  `firstname` char(30) NOT NULL,
-  `name` char(30) NOT NULL,
+  `firstname` char(30) DEFAULT NULL,
+  `name` char(30) DEFAULT NULL,
   `token` char(30) DEFAULT NULL,
-  `address` char(50) NOT NULL,
-  `city` char(50) NOT NULL,
-  `cp` char(5) NOT NULL,
-  `password` char(100) NOT NULL,
+  `address` char(50) DEFAULT NULL,
+  `city` char(50) DEFAULT NULL,
+  `cp` char(5) DEFAULT NULL,
+  `password` char(100) DEFAULT NULL,
   `weight` double DEFAULT NULL,
   `height` double DEFAULT NULL,
   `calorie` int(11) DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `email` char(50) NOT NULL
+  `email` char(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `detail_sport`
+--
+ALTER TABLE `detail_sport`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `ingredient`
@@ -108,6 +142,12 @@ ALTER TABLE `ingredient`
 -- Index pour la table `ingredient_detail`
 --
 ALTER TABLE `ingredient_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `plan_sport`
+--
+ALTER TABLE `plan_sport`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -127,6 +167,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `detail_sport`
+--
+ALTER TABLE `detail_sport`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `ingredient`
 --
 ALTER TABLE `ingredient`
@@ -136,6 +182,12 @@ ALTER TABLE `ingredient`
 -- AUTO_INCREMENT pour la table `ingredient_detail`
 --
 ALTER TABLE `ingredient_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `plan_sport`
+--
+ALTER TABLE `plan_sport`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
