1f90249ff86d034d7e503ed11ca1ee1c2fad52c4
ThaMad <thanudi.m@gmail.com>
2023-03-08 07:02:34 +0100

    Traduction

--- ConnectAPI/ConnectAPI/MainPages/Nutrition/IngredientsViewController.swift
+++ ConnectAPI/ConnectAPI/MainPages/Nutrition/IngredientsViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class IngredientsViewController: UIViewController {
+class IngredientsViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var todayLbl: UILabel!
+    @IBOutlet weak var createBtn: UIButton!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var quantityTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addIngredientsLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.nameTextField.delegate = self
+        self.quantityTextField.delegate = self
+        self.weightTextField.delegate = self
+        
+        todayLbl.text = NSLocalizedString("topbar.today", comment: "")
+        nameTextField.placeholder = NSLocalizedString("controllers.mainpages.nutrition.ingredientdetail.name", comment: "")
+        quantityTextField.placeholder = NSLocalizedString("controllers.mainpages.nutrition.ingredientdetail.quantity", comment: "")
+        weightTextField.placeholder = NSLocalizedString("controllers.mainpages.ingredientdetail.weight", comment: "")
+        
+        createBtn.setTitle(NSLocalizedString("controllers.mainpages.nutrition.ingredientdetail.createbutton", comment: ""), for: .normal)
+        
+        addIngredientsLbl.text = NSLocalizedString("controllers.mainpages.nutrition.ingredientdetail.createlabel", comment: "")
+        
+        super.viewDidLoad()
+        
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
