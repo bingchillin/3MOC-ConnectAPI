2b628c5e0f6d3cff9a4e1c83a8634cf1e6769fcc
ThaMad <thanudi.m@gmail.com>
2023-03-08 09:45:01 +0100

    traduction

--- ConnectAPI/ConnectAPI/MainPages/Homepage/HomepageViewController.swift
+++ ConnectAPI/ConnectAPI/MainPages/Homepage/HomepageViewController.swift
@@ -9,13 +9,38 @@ import UIKit
 
 class HomepageViewController: UIViewController {
     
+    @IBOutlet weak var recipeLbl: UILabel!
+    @IBOutlet weak var programLbl: UILabel!
+    @IBOutlet weak var suggestLbl: UILabel!
+    @IBOutlet weak var todayLbl: UILabel!
+    @IBOutlet weak var logoutBtn: UIButton!
     class func newInstance() -> HomepageViewController {
         let homepageViewController = HomepageViewController()
         
         return homepageViewController
     }
 
+    @IBAction func handleLogout(_ sender: UIButton) {
+        
+            if (UserDefaults.standard.string(forKey: "uId") != nil){
+                let parameter = "{\n    \"id\" : \(UserDefaults.standard.string(forKey: "uId")!)\n"
+                UserWebService.logoutUser(parameters: parameter)
+                self.navigationController?.pushViewController(HomeViewController(), animated: true)
+                
+            }
+            
+            else{
+                print("MARCHE PAS")
+            }
+            
+    }
     override func viewDidLoad() {
+        
+        todayLbl.text = NSLocalizedString("topbar.today", comment: "")
+        suggestLbl.text = NSLocalizedString("controllers.mainpages.homepage", comment: "")
+        programLbl.text = NSLocalizedString("controllers.mainpages.training.program", comment: "")
+        recipeLbl.text = NSLocalizedString("controllers.mainpages.nutrition.recipelabel", comment: "")
+        logoutBtn.setTitle(NSLocalizedString("topbar.logout", comment: ""), for: .normal)
         super.viewDidLoad()
         /*
         if let tabBarItemHome = self.tabBarController?.tabBar.items?[0] {
